d354a6d111ee9b62a08d4df794174628592f6e77
Austin DeLauney <austin@Austins-MacBook-Pro.local>
2017-04-26 17:15:14 -0400

    RSA works thanks to random dude on internet

--- rsa_gen.py
+++ rsa_gen.py
@@ -1,18 +1,20 @@
-from miller_rabin import miller_rabin
+from miller_rabin import isPrime
+from math import gcd
 import random
 random.seed()
 
 def get_prime():
     a = random.getrandbits(512)
-    while(not miller_rabin(a)):
+    while(not isPrime(a)):
         a = random.getrandbits(512)
 
+    return a
 p = get_prime()
 q = get_prime()
 phi = (p-1) * (q-1)
 n = p*q
 b = random.randrange(2, phi-1)
-while gcd(b,z) != 1:
+while gcd(b,n) != 1:
     b = random.randrange(2,phi-1)
 print(p)
 print(q)
